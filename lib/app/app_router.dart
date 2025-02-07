97d3bf734a6caa5ce19835e1cb96212d2c3d64cd
gideonotieno <bruceotieno25@gmail.com>
2025-02-07 12:17:40 +0300

    ###

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_realtime_object_detection/pages/home_screen.dart';
 import 'package:flutter_realtime_object_detection/pages/local_screen.dart';
 import 'package:flutter_realtime_object_detection/pages/splash_screen.dart';
-import 'package:flutter_realtime_object_detection/services/navigation_service.dart';
 import 'package:flutter_realtime_object_detection/services/tensorflow_service.dart';
 import 'package:flutter_realtime_object_detection/view_models/home_view_model.dart';
 import 'package:flutter_realtime_object_detection/view_models/local_view_model.dart';
@@ -14,12 +13,8 @@ class AppRoute {
   static const localScreen = '/localScreen';
 
   static final AppRoute _instance = AppRoute._private();
-  factory AppRoute() {
-    return _instance;
-  }
+  factory AppRoute() => _instance;
   AppRoute._private();
-
-  static AppRoute get instance => _instance;
 
   static Widget createProvider<P extends ChangeNotifier>(
       P Function(BuildContext context) provider,
@@ -27,9 +22,7 @@ class AppRoute {
       ) {
     return ChangeNotifierProvider<P>(
       create: provider,
-      builder: (_, __) {
-        return child;
-      },
+      child: child,
     );
   }
 
@@ -37,28 +30,39 @@ class AppRoute {
     switch (settings.name) {
       case splashScreen:
         return AppPageRoute(builder: (_) => SplashScreen());
+
       case homeScreen:
         Duration? duration;
-        if (settings.arguments != null) {
+        if (settings.arguments is Map<String, dynamic>) {
           final args = settings.arguments as Map<String, dynamic>;
-          if ((args['isWithoutAnimation'] as bool)) {
+          if (args['isWithoutAnimation'] is bool && args['isWithoutAnimation']) {
             duration = Duration.zero;
           }
         }
         return AppPageRoute(
-            appTransitionDuration: duration,
-            appSettings: settings,
-            builder: (_) => ChangeNotifierProvider(
-                create: (context) => HomeViewModel(context,
-                    Provider.of<TensorFlowService>(context, listen: false)),
-                builder: (_, __) => HomeScreen()));
+          appTransitionDuration: duration,
+          appSettings: settings,
+          builder: (context) => ChangeNotifierProvider(
+            create: (context) => HomeViewModel(
+              context,
+              Provider.of<TensorFlowService>(context, listen: false),
+            ),
+            child: HomeScreen(),
+          ),
+        );
+
       case localScreen:
         return AppPageRoute(
-            appSettings: settings,
-            builder: (_) => ChangeNotifierProvider(
-                create: (context) => LocalViewModel(context,
-                    Provider.of<TensorFlowService>(context, listen: false)),
-                builder: (_, __) => LocalScreen()));
+          appSettings: settings,
+          builder: (context) => ChangeNotifierProvider(
+            create: (context) => LocalViewModel(
+              context,
+              Provider.of<TensorFlowService>(context, listen: false),
+            ),
+            child: LocalScreen(),
+          ),
+        );
+
       default:
         return null;
     }
@@ -66,20 +70,16 @@ class AppRoute {
 }
 
 class AppPageRoute extends MaterialPageRoute<Object> {
-  Duration? appTransitionDuration;
+  final Duration? appTransitionDuration;
+  final RouteSettings? appSettings;
 
-  RouteSettings? appSettings;
-
-  AppPageRoute(
-      {required WidgetBuilder builder,
-        this.appSettings,
-        this.appTransitionDuration})
-      : super(builder: builder);
+  AppPageRoute({
+    required WidgetBuilder builder,
+    this.appSettings,
+    this.appTransitionDuration,
+  }) : super(builder: builder, settings: appSettings ?? const RouteSettings());
 
   @override
   Duration get transitionDuration =>
       appTransitionDuration ?? super.transitionDuration;
-
-  @override
-  RouteSettings get settings => appSettings ?? super.settings;
 }
